3cc4554686e60208f18a651c93337d17cf698254
LexBrown <44871845+lexbrown@users.noreply.github.com>
2018-12-13 11:59:20 +0300

    Add files via upload

---
+++ localminimums.py
@@ -0,0 +1,14 @@
+#Программа получает дискретную эмпирическую последовательность
+#и считает локальные минимумы
+
+seq =[]
+locmin = []
+for i in 2:
+	a = int(input())
+	seq.append(a)
+	print(i)	#отладчик
+while true:
+	a = int(input())
+	seq.append(a)
+	if seq[-2] < seq[-3] and seq[-2] < seq[-1]:
+		locmin.append(seq[-2])
