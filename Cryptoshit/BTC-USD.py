2ab432c293af3cb4b16329f4808b391f784242dc
LexBrown <44871845+lexbrown@users.noreply.github.com>
2019-01-11 01:06:59 +0300

    Update BTC-USD.py

--- Cryptoshit/BTC-USD.py
+++ Cryptoshit/BTC-USD.py
@@ -4,12 +4,15 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-btc = pd.read_csv("BTC_USD Bitfinex Historical Data", index_col = 0, parse_dates = True)
+btc = pd.read_excel("BTC-USD - final dataset.xlsx", index_col = 0, parse_dates = True)
 btc_price = btc["Price"]
 fig = plt.figure(figsize = (16, 8))
 subfig = fig.add_subplot(1, 1, 1)
 subfig.set_ylabel("Price", fontsize=20)
 subfig.set_xlabel("Date", fontsize=20)
-btc_price.plot(ax = subfig)
-subfig.add_title("BTC-USD", fontsize=30)
+btc_price.plot(ax = subfig, color = "b", label = 'BTC-USD')
+btc_price.rolling(window = 20).mean().plot(ax = subfig, label = "MA (20)", color = "r", linestyle = "-." )
+btc_price.ewm(span = 50).mean().plot(ax = subfig, label = "EWMA (50)", color = "g", linestyle = "--" )
+subfig.set_title("BTC-USD", fontsize=30)
+subfig.legend(loc = "best")
 fig.savefig("BTC-USD (2017-2018).png")
