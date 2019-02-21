d1173c1c4413eb12f786a83193dacdaf607fdadb
LexBrown <44871845+lexbrown@users.noreply.github.com>
2019-02-21 11:18:43 +0300

    Create Test OLS.py

---
+++ Test OLS.py
@@ -0,0 +1,22 @@
+# Test OLS
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import statsmodels.api as sm
+
+raw_set = pd.read_excel('OLS_testset.xlsx')
+figols =plt.figure(figsize = (10, 5))
+subfigols = figols.add_subplot(1,1,1)
+dep_v = raw_set['y']
+indep_v = raw_set['x']
+
+subfigols.scatter(x = indep_v, y = dep_v)
+
+x_reg = sm.add_constant(indep_v)
+results = sm.OLS(dep_v, x_reg).fit()
+b0, b1 = results.params
+
+subfigols.plot(indep_v, b0 + b1 * indep_v)
+
+print(results.summary())
